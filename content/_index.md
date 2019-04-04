f794c9f2ff343eb3f826cd6fb849585570f560ee
Francis Laguna-Driscoll <fldriscoll@gmail.com>
2019-04-04 11:49:10 -0400

    Switch to Bootstrap
    Switching to Bootstrap from uikit.
    Reason: Better documentation, html/css format.
    
    Downside: no more prebuilt animations, parallax, carousels, which are very nice features that uikit has, but they are a bit buggy

--- content/_index.md
+++ content/_index.md
@@ -1,7 +1,8 @@
-+++
-date = "2015-06-24T13:50:46+02:00"
-title = "Home"
-[menu.main]
-weight = 1
+---
+date: '2015-06-24T13:50:46+02:00'
+title: Home
+menu:
+  main:
+    weight: 1
 
-+++
+---
--- themes/strata/static/js/main.js
+++ themes/strata/static/js/main.js
@@ -1,12 +1,16 @@
-$('.js-tilt').tilt({
-    maxTilt:        10,
-    perspective:    2000,   // Transform perspective, the lower the more extreme the tilt gets.
-    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-    scale:          1.05,      // 2 = 200%, 1.5 = 150%, etc..
-    speed:          900,    // Speed of the enter/exit transition.
-    transition:     true,   // Set a transition on enter/exit.
-    disableAxis:    null,   // What axis should be disabled. Can be X or Y.
-    reset:          false,   // If the tilt effect has to be reset on exit.
-    glare:          false,  // Enables glare effect
-    maxGlare:       1       // From 0 - 1.
-})
+
+AOS.init();
+$(document).ready(function(){
+    $('.js-tilt').tilt({
+        maxTilt:        10,
+        perspective:    2000,   // Transform perspective, the lower the more extreme the tilt gets.
+        easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+        scale:          1.03,      // 2 = 200%, 1.5 = 150%, etc..
+        speed:          900,    // Speed of the enter/exit transition.
+        transition:     true,   // Set a transition on enter/exit.
+        disableAxis:    null,   // What axis should be disabled. Can be X or Y.
+        reset:          false,   // If the tilt effect has to be reset on exit.
+        glare:          false,  // Enables glare effect
+        maxGlare:       1       // From 0 - 1.
+    })
+});
