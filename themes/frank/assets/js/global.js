856a14e37004403b003af19dac8cfc632e2bcb32
Francis Laguna-Driscoll <fldriscoll@gmail.com>
2019-04-17 23:10:56 -0400

    dfadfasdf

--- themes/frank/assets/js/global.js
+++ themes/frank/assets/js/global.js
@@ -1,6 +1,33 @@
 
 $(document).ready(function(){
 
+    $('.js-tilt').tilt({
+        maxTilt:        10,
+        perspective:    2000,   // Transform perspective, the lower the more extreme the tilt gets.
+        easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+        scale:          1.03,      // 2 = 200%, 1.5 = 150%, etc..
+        speed:          900,    // Speed of the enter/exit transition.
+        transition:     true,   // Set a transition on enter/exit.
+        disableAxis:    null,   // What axis should be disabled. Can be X or Y.
+        reset:          false,   // If the tilt effect has to be reset on exit.
+        glare:          false,  // Enables glare effect
+        maxGlare:       1       // From 0 - 1.
+    })
+        
+    $('.js-tilt-lg').tilt({
+        maxTilt:        10,
+        perspective:    3000,   // Transform perspective, the lower the more extreme the tilt gets.
+        easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+        scale:          1.01,      // 2 = 200%, 1.5 = 150%, etc..
+        speed:          900,    // Speed of the enter/exit transition.
+        transition:     true,   // Set a transition on enter/exit.
+        disableAxis:    null,   // What axis should be disabled. Can be X or Y.
+        reset:          false,   // If the tilt effect has to be reset on exit.
+        glare:          false,  // Enables glare effect
+        maxGlare:       1       // From 0 - 1.
+    })
+
+
 let toggler = document.querySelectorAll('.toggler');
 let sidenav = document.getElementById('mySidenav');
 let items = document.querySelectorAll('.nav-item');
