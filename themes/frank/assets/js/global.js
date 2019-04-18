ceb68bec511bcc32b402701407852449a6ba7d29
Francis Laguna-Driscoll <fldriscoll@gmail.com>
2019-04-17 20:20:50 -0400

    so much stuff!!!

---
+++ themes/frank/assets/js/global.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+
+let toggler = document.querySelectorAll('.toggler');
+let sidenav = document.getElementById('mySidenav');
+let items = document.querySelectorAll('.nav-item');
+let header = document.querySelectorAll('.menu-header');
+
+    TweenMax.to( '.navitems' , 1, { x:'200px' } );
+    toggler.forEach(function(t , k) {
+        t.addEventListener("mousedown", function(){ 
+            sidenav.classList.toggle('menu-active');
+            if (k === 0) {
+                TweenMax.staggerFrom( header  , .5, { x: -100 , opacity: 0 }, 0.2);
+                TweenMax.staggerFrom( items  , .5, { x: -100 , opacity: 0, delay: .5 }, 0.08);
+            }
+            else {
+                
+            }
+         });
+    });
+
+
+    
+});
