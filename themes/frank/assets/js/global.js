4e8d4f024524c9f94886d88022b0667481c119fd
Francis Laguna-Driscoll <fldriscoll@gmail.com>
2019-04-19 13:04:41 -0400

    dfZXcZ

--- themes/frank/assets/js/global.js
+++ themes/frank/assets/js/global.js
@@ -5,7 +5,6 @@ let header = document.querySelectorAll('.menu-header');
 let logo = document.getElementById('logo');
 let letters = document.querySelectorAll('.logostagger');
 
-    TweenMax.to( '.navitems' , 1, { x:'200px' } );
     toggler.forEach(function(t , k) {
         t.addEventListener("mousedown", function(){ 
             sidenav.classList.toggle('menu-active');
@@ -14,13 +13,10 @@ let letters = document.querySelectorAll('.logostagger');
                 TweenMax.staggerFrom( items  , .5, { x: -100 , opacity: 0, delay: .5 }, 0.08);
             }
             else {
-                TweenMax.staggerFrom( header  , .1, { x: -100 , opacity: 0 }, 0.1);
-                TweenMax.staggerFrom( items  , .1, { x: -100 , opacity: 0 }, 0.08);
-            }
-         });
-    });
-    var logotl = new TimelineMax({paused: true});
-    logotl.staggerTo( letters  , .3, { y: "-=5px" }, 0.05);
+            menutl.reverse();
+        }
+     });
+});
 
     $('#logo').hover(function(){
             logotl.play();
