945c4348b732bb578d823ee29fbf8ffc17cb3ec3
Francis Laguna-Driscoll <fldriscoll@gmail.com>
2019-04-18 22:51:31 -0400

    Added Functionality

--- themes/frank/assets/js/global.js
+++ themes/frank/assets/js/global.js
@@ -1,37 +1,9 @@
-
-$(document).ready(function(){
-
-    $('.js-tilt').tilt({
-        maxTilt:        10,
-        perspective:    2000,   // Transform perspective, the lower the more extreme the tilt gets.
-        easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-        scale:          1.03,      // 2 = 200%, 1.5 = 150%, etc..
-        speed:          900,    // Speed of the enter/exit transition.
-        transition:     true,   // Set a transition on enter/exit.
-        disableAxis:    null,   // What axis should be disabled. Can be X or Y.
-        reset:          false,   // If the tilt effect has to be reset on exit.
-        glare:          false,  // Enables glare effect
-        maxGlare:       1       // From 0 - 1.
-    })
-        
-    $('.js-tilt-lg').tilt({
-        maxTilt:        10,
-        perspective:    3000,   // Transform perspective, the lower the more extreme the tilt gets.
-        easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-        scale:          1.01,      // 2 = 200%, 1.5 = 150%, etc..
-        speed:          900,    // Speed of the enter/exit transition.
-        transition:     true,   // Set a transition on enter/exit.
-        disableAxis:    null,   // What axis should be disabled. Can be X or Y.
-        reset:          false,   // If the tilt effect has to be reset on exit.
-        glare:          false,  // Enables glare effect
-        maxGlare:       1       // From 0 - 1.
-    })
-
-
 let toggler = document.querySelectorAll('.toggler');
 let sidenav = document.getElementById('mySidenav');
 let items = document.querySelectorAll('.nav-item');
 let header = document.querySelectorAll('.menu-header');
+let logo = document.getElementById('logo');
+let letters = document.querySelectorAll('.logostagger');
 
     TweenMax.to( '.navitems' , 1, { x:'200px' } );
     toggler.forEach(function(t , k) {
@@ -47,7 +19,13 @@ let header = document.querySelectorAll('.menu-header');
             }
          });
     });
+    var logotl = new TimelineMax({paused: true});
+    logotl.staggerTo( letters  , .3, { y: "-=5px" }, 0.05);
 
-
-    
-});
+    $('#logo').hover(function(){
+            logotl.play();
+        },
+        function(){
+            logotl.reverse();
+        }
+    );
